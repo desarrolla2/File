3d15cbc9f08ff3a7ed270e5ad650f4a5d89939ef
Daniel González Cerviño <daniel@desarrolla2.com>
2015-01-23 11:30:48 +0100

    Initial import

---
+++ src/File.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the marvel package.
+ *
+ * Copyright (c) Daniel González
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Daniel González <daniel@desarrolla2.com>
+ */
+
+namespace Desarrolla2\File;
+
+use RuntimeException;
+
+/**
+ * File
+ */
+class File
+{
+    /**
+     * @param string $fileName
+     *
+     * @return string
+     * @throws RuntimeException
+     */
+    public static function read($fileName)
+    {
+        if (!file_exists($fileName)) {
+            throw new RuntimeException($fileName.' not exist');
+        }
+        if (!is_readable($fileName)) {
+            throw new RuntimeException($fileName.' not readable');
+        }
+
+        $fh = fopen($fileName, "r");
+        $data = fread($fh, filesize($fileName));
+        fclose($fh);
+
+        return $data;
+    }
+
+    /**
+     * @param string $fileName
+     * @param string $data
+     *
+     * @throws RuntimeException
+     */
+    public static function write($fileName, $data)
+    {
+        if (!file_exists($fileName)) {
+            if (!is_writable(dirname($fileName))) {
+                throw new RuntimeException($fileName.' not writable');
+            }
+        } else {
+            if (!is_writable($fileName)) {
+                throw new RuntimeException($fileName.' not writable');
+            }
+        }
+
+        $handler = fopen($fileName, "w");
+        fwrite($handler, (string) $data);
+        fclose($handler);
+    }
+}
---
+++ src/Json.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the marvel package.
+ *
+ * Copyright (c) Daniel González
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Daniel González <daniel@desarrolla2.com>
+ */
+
+namespace Desarrolla2\File;
+
+use RuntimeException;
+
+/**
+ * Json
+ */
+class Json extends File
+{
+    /**
+     * @param string $fileName
+     *
+     * @return array
+     * @throws RuntimeException
+     */
+    public static function read($fileName)
+    {
+        $data = parent::read($fileName);
+
+        $json = json_decode($data, true);
+
+        if (json_last_error()) {
+            throw new \RuntimeException(json_last_error_msg());
+        }
+
+        return $json;
+    }
+
+    /**
+     * @param string $fileName
+     * @param array  $data
+     *
+     * @throws RuntimeException
+     */
+    public static function write($fileName, $data)
+    {
+        $json = json_encode($data, JSON_PRETTY_PRINT);
+
+        if (json_last_error()) {
+            throw new \RuntimeException(json_last_error_msg());
+        }
+
+        parent::write($fileName, $json);
+    }
+}
